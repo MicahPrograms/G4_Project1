199e9a95e4aa32b2dd1f512a1f02be93e7f37e5e
Micah Brown <brown_micah@outlook.com>
2022-12-05 13:04:17 -0700

    updated files

--- public/formValidation.js
+++ public/formValidation.js
@@ -3,13 +3,11 @@ function confirm_reset() {
 }
 //Form validation from Micah
 
-
-
-var formLength = 10;
+var creditCardNum = document.getElementById("Credit").value;
 
 function validate(myForm) {
     console.log("made it into the function");
-
+    var formLength = 10;
     for (i=0; i<formLength; i++) {
         console.log(myForm[i].name);
 
@@ -20,65 +18,6 @@ function validate(myForm) {
             return false;
         }
     }
-
-
-
-
-
-
-
-/*
-function validate(myForm)
-{
-    var FirstName = document.getElementById("FirstName");
-    if (FirstName.value == "")
-    {
-        alert("You must provide your first name");
-        FirstName.focus();
-        return false;
-    }
-
-    var LastName = document.getElementById("LastName");
-    if (LastName.value == "")
-    {
-        alert("You must provide your last name");
-        LastName.focus();
-        return false;
-    }
-
-    var HomeAddress = document.getElementById("HomeAddress");
-    if (HomeAddress.value == "")
-    {
-        alert("You must provide your Home Address");
-        HomeAddress.focus();
-        return false;
-    }
-
-    var ProvState = document.getElementById("ProvState");
-    if (ProvState.value == "")
-    {
-        alert("You must provide your Province/State");
-        ProvState.focus();
-        return false;
-    }
-
-    var Country = document.getElementById("Country");
-    if (Country.value == "")
-    {
-        alert("You must provide your Country of residence");
-        Country.focus();
-        return false;
-    }
-
-    var Email = document.getElementById("Email");
-    if (Email.value == "")
-    {
-        alert("You must provide your Email Address (so we can send you spam)");
-        Email.focus();
-        return false;
-    }
-
-    */
 
 
     // - Micah: Comparing credit card number to regular expressions for each majot credit card type to make sure card is valid 
@@ -97,8 +36,8 @@ function validate(myForm)
     
     //Expiry date test
     var regExp = /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/;
-    var ExpiryDate = document.getElementById("ExpiryDate").value;
-    if(!regExp.test(ExpiryDate))
+    var Expiry = document.getElementById("ExpiryDate").value;
+    if(!regExp.test(Expiry))
     {
         alert("Please enter a valid Expiry Date.");
         ExpiryDate.focus();
@@ -107,25 +46,27 @@ function validate(myForm)
 
 return confirm("Continue submitting?");
 
-
-
+}
 
     function cardIdentify(){
+        var creditCardNum = document.getElementById("Credit").value;
         var regMaster = /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/;
         var regVisa = /^4[0-9]{12}(?:[0-9]{3})?$/;
         var regAMEX = /^3[47][0-9]{13}$/;
     
         if(regMaster.test(creditCardNum)) {
-            alert("Mastercard!");
+            document.getElementById('card').className = "cardvisible";
+            document.getElementById('card').src = "mastercard.png";
             return false;
         }
-        if(regVisa.test(creditCardNum)) {
-            alert("Visa!");
+        if(regVisa.test(creditCardNum) == true) {
+            document.getElementById('card').className = "cardvisible";
+            document.getElementById('card').src = "visa.png";
             return false;
         }
-        if(regAMEX.test(creditCardNum)) {
-            alert("American Express!");
+        if(regAMEX.test(creditCardNum) == true) {
+            document.getElementById('card').className = "cardvisible";
+            document.getElementById('card').src = "amex.png";
             return false;
         }
     } 
-}
