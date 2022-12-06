63743ead87bc56c1c48911ae0cdcbdfbcad392d1
Micah Brown <brown_micah@outlook.com>
2022-12-06 10:02:00 -0700

    added form post method to server page

--- ServerCode.js
+++ ServerCode.js
@@ -114,21 +114,21 @@ app.get("/orders4", (req, res) => {
 	});   
 });
 
-app.get("/Confirmation", (req, res) => {
-    var myConnection = getConnection();
-	myConnection.connect((err)=>{
-		if (err) throw err;
-		myConnection.query("SELECT * FROM packages", (err,result,fields)=>{
-			if (err) throw err;
-			console.log(result);
-			console.log(fields);
-			res.render("Confirmation", { "result": result, "fields": fields });
-			myConnection.end((err)=>{
-				if (err) throw err;
-			});
-		});
-	});   
-});
+// app.get("/Confirmation", (req, res) => {
+//     var myConnection = getConnection();
+// 	myConnection.connect((err)=>{
+// 		if (err) throw err;
+// 		myConnection.query("SELECT * FROM packages", (err,result,fields)=>{
+// 			if (err) throw err;
+// 			console.log(result);
+// 			console.log(fields);
+// 			res.render("Confirmation", { "result": result, "fields": fields });
+// 			myConnection.end((err)=>{
+// 				if (err) throw err;
+// 			});
+// 		});
+// 	});   
+// });
 
 app.get("/contact", (req, res) => {
     var myConnection = getConnection();
@@ -161,6 +161,14 @@ app.get("/getallagents", (req, res)=>{
 		});
 	});
 });
+
+
+app.post("/register", function(req, res, next) {
+	console.log(req.body);
+	res.send(req.body);
+})
+
+
 // app.use("/contact",(req,res)=>{
 //     res.render("contact");
 // });
