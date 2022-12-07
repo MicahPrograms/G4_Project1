c28ddb0e4bc42a472d6afbc6250202977b85ec2a
Tim Huynh <timhuynh12@gmail.com>
2022-12-06 21:17:39 -0700

    updated to fix all packages

--- ServerCode.js
+++ ServerCode.js
@@ -20,8 +20,8 @@ function getConnection()
 {
 	var con = mysql.createConnection({
 		host: "localhost",
-		user: "Micah",
-		password: "Micah3cheese",
+		user: "tim",
+		password: "password",
 		database: "travelexperts"
 	});
 	return con;
